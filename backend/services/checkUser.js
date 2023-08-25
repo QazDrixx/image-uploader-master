810b568a166940863b4582f5ba1901970ba36d10
QazDrixx <pugach0548@gmail.com>
2023-08-25 19:47:38 +0300

    modified:   services/checkUser.js

--- backend/services/checkUser.js
+++ backend/services/checkUser.js
@@ -4,14 +4,15 @@ import { config } from "dotenv";
 config({path: '../../.env'})
 
 export const checkUser = (req, res, next) => {
+    const token = (req.headers.authorization || '').replace(/Bearer /, '')
+    if (!token) return res.status(403).json({msg: "Unauthorized user"})
+    console.log(token);
     try {
-        const token = (req.headers.authorization || '').replace(/Bearer /, '')
-        if (!token) throw new Error('No access')
         req.userId = jwt.verify(token, process.env.SECRET_KEY).userId
         next()
         
     } catch (err) {
         console.log(err);
-        res.status(403).json({error:err.message})
+        res.status(403).json({msg: "Invalid token"})
     }
 }
