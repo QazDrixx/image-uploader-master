8233c674306fbbdb9279b0951a0e9a9f5ddd66c0
QazDrixx <pugach0548@gmail.com>
2023-10-28 21:27:07 +0300

    modified:   backend/services/storage.js

--- backend/services/storage.js
+++ backend/services/storage.js
@@ -16,6 +16,7 @@ export const storage = multer.diskStorage({
     },
 
     filename: (req, file, cb) => {
+        file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
         cb(null, `${randomUUID()}${parse(file.originalname).ext}`)
     },
 
