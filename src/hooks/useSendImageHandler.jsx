950c863352a541bc05d69b74d2e6f98a5e27a6f4
QazDrixx <pugach0548@gmail.com>
2023-11-13 18:59:22 +0200

    modified:   src/hooks/useSendImageHandler.jsx

--- src/hooks/useSendImageHandler.jsx
+++ src/hooks/useSendImageHandler.jsx
@@ -1,21 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setLoadingImage } from '../redux/submitSlice';
+import { useSelector } from 'react-redux';
 
 export const useSendImageHandler = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const isAuth = useSelector(state => state.auth.isAuth)
 
     const sendImageHandler = async (callback) => {
         try {
             dispatch(setLoadingImage(true))
+            if (!isAuth) {
+                navigate('registration/')
+            }
             const response = await callback();
             if (response.status === 200) {
                 if (response.data.length === 1) navigate(`image/${response.data[0]['_id']}`)
                 else navigate(`multipleUploaded/`, {state: response.data})
-            }
-            else if (response.status < 500) {
-                navigate('registration/')
             }
             // navigate()
         
