4763685b60ab31748cbee2f03b215534c4587b79
QazDrixx <pugach0548@gmail.com>
2024-04-18 13:50:27 +0300

    modified:   backend/controllers/uploadFile.js 	modified:   backend/services/storage.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    vercel.json

--- backend/services/storage.js
+++ backend/services/storage.js
@@ -6,7 +6,7 @@ import { parse } from 'node:path'
 const uploadFolder = 'media/'
 export const storage = multer.diskStorage({
     destination: async (req, file, cb) => {
-        const path = `${uploadFolder}${getFolderName()}`
+        const path = `${uploadFolder}`
         try {
             await mkdir(path, {recursive:true}, (err) => {if(err) throw err})
         } catch (err){
@@ -25,10 +25,4 @@ export const storage = multer.diskStorage({
 export const fileFilter = (req, file, cb) =>{
     if (file.mimetype.includes('image')) cb(null, true)
     else cb(null, false)
-}
-
-function getFolderName() {
-    const date = new Date()
-    const addZero = (num) => num > 9 ? num : `0${num}`;
-    return `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}-${addZero(date.getHours())}-${addZero(date.getMinutes())}_${randomUUID()}`
 }
